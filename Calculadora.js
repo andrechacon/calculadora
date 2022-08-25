3dea36e3de9f367eed12ddde496cedef9e7b8fd7
andrechacon <achacon190@gmail.com>
2022-08-24 23:53:22 -0300

    commit1

---
+++ Calculadora.js
@@ -0,0 +1,6 @@
+const sumar = require('./Funciones/Sumar')
+const restar = require('./Funciones/Restar')
+const dividir = require('./Funciones/Dividir')
+const multiplicar = require('./Funciones/Multiplicar')
+
+console.log(dividir(45,0))
